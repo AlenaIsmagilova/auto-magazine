09bb8cc3f6d07314df3d1f4bf54aaff2e3cd8d7b
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2022-04-01 13:19:27 +0300

    added adaptive layout for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 const galleryButtonBack = document.querySelector(".content__button-left");
 const galleryButtonForward = document.querySelector(".content__button-right");
 const galleryContainer = document.querySelector(".content__gallery-container");
+const humburgerMenuButton = document.querySelector(".humburger-menu__button");
+const humburgerMenu = document.querySelector('.humburger_menu');
 
 galleryButtonForward.addEventListener("click", () => {
-  galleryContainer.style.setProperty("transform", "translateX(-101%)");
+  galleryContainer.style.setProperty("transform", "translateX(-100%)");
 });
 
 galleryButtonBack.addEventListener("click", () => {
   galleryContainer.style.setProperty("transform", "translateX(0)");
 });
+
+humburgerMenuButton.addEventListener('click', () => {
+humburgerMenu.style.
+})
